f056d841ec506b0a7e963504834f1192ad829fbd
Iradukunda-Fils <iradukundafils1@gmail.com>
2024-12-09 20:14:38 +0200

    directory migration

--- user_management/urls.py
+++ user_management/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
 ]
 
 # Serve static and media files in development
-if settings.DEBUG:
+if settings.DEBUG is False:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
